e464d49deef59b72291810f8ddec1edea81dff2e
Gagandheep Raikar <32958460+Gagandheep@users.noreply.github.com>
2021-06-28 18:24:06 +0530

    changed process to thread

--- leads/processes.py
+++ leads/processes.py
@@ -1,4 +1,5 @@
 from multiprocessing import Process
+from threading import Thread
 from pandas import read_csv
 from telethon import events
 from telethon import TelegramClient
@@ -7,12 +8,16 @@ from leads.models import TelegramMessage
 from time import sleep
 from threading import Thread
 from asgiref.sync import sync_to_async, async_to_sync
+import asyncio
 
 
 def job(sess_str, ph_no):
 
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
     client = TelegramClient(StringSession(sess_str),
-                            1868530, "edf7d1e794e0b4a5596aa27c29d17eba")
+                            1868530, "edf7d1e794e0b4a5596aa27c29d17eba", 
+                            loop=loop)
 
     @sync_to_async
     def update_message(message):
@@ -29,6 +34,7 @@ def job(sess_str, ph_no):
     @client.on(events.NewMessage())
     async def handler(event):
         message = event.message
+        print(message)
         await update_message(message)
 
     with client:
@@ -37,15 +43,17 @@ def job(sess_str, ph_no):
 
 sessions = read_csv('leads/sessions.csv')
 
-processes = []
+threads = []
 
 for _, session in sessions.iterrows():
     session_str = session['sess_str1']
     number = session['number']
-    processes.append(Process(target=job, args=(session_str, number)))
+    threads.append(Thread(target=job, args=(session_str, number)))
 
-for process in processes:
-    process.start()
+for thread in threads:
+    thread.start()
 
-for process in processes:
-    process.join()
+print("threads spawned")
+
+for thread in processes:
+    thread.join()
